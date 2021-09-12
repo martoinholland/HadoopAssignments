407ceb307235265ae7670867e4437ecbe3893d42
martoinholland <42969166+martoinholland@users.noreply.github.com>
2021-09-13 02:24:51 +0300

    Added comments

--- Count_Movie_Ratings.py
+++ Count_Movie_Ratings.py
@@ -1,20 +1,26 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
+#define a class for the job we want to execute
 class RatingsBreakdownPerMovie (MRJob):
+    
+    #define the steps of our job
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_movies,
                    reducer=self.reducer_count_ratings)
         ]
-
+    
+    #define a mapper which will map the movie id's to their ratings. 
+    #We are returning the movie ID and 1, since we are interested in counting the number of ratings and not the rating's values themselves.
     def mapper_get_movies(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
         yield movieID, 1
 
+    #define a reducer which will count all of the ratings for each movie
     def reducer_count_ratings (self, key, values):
         yield key,sum(values)
 
-
+#run the code above
 if __name__ == '__main__':
     RatingsBreakdownPerMovie.run()
